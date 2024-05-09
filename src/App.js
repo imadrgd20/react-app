d62b68b87464906dd48fb63aa4ed68f0cbc7357d
imadrgd20 <reguadiimad@gmail.com>
2024-05-09 17:56:46 +0100

    hello

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import MarkdownEditor from "./MarkDown/MarkDown";
+import PomodoroTimer from "./PomodoroTimer/PomodoroTimer";
+import DrumMachine from "./DrumMAchine/DrumMachine";
+import Calculatrice from "./Calculatrice/Calculater";
+
+
+
+  export default function App(){
+
+    return (
+
+        <>
+       <Calculatrice/>
+        </>
+        
+    );
+  }
+
+
